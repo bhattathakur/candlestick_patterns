859eb3cda3e454db898192c80d7886b3c1e4e061
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-22 00:04:02 -0500

    new try

--- pythonfiles/plotlyplot.py
+++ pythonfiles/plotlyplot.py
@@ -90,5 +90,16 @@ def get_respective_pattern(temp_df,pattern_name):
 #fig update
   fig.update_layout(showlegend=True)
   fig.update_layout(hovermode='x unified')
+  fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
 
   return fig
