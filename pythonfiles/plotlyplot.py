b405a89f8cc1320cbf1c9963aa8e4f17df844fc9
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-22 00:08:22 -0500

    added some step backs

--- pythonfiles/plotlyplot.py
+++ pythonfiles/plotlyplot.py
@@ -96,10 +96,11 @@ def get_respective_pattern(temp_df,pattern_name):
     rangeselector=dict(
         buttons=list([
             dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=3, label="3m", step="month", stepmode="backward"),
             dict(count=6, label="6m", step="month", stepmode="backward"),
             dict(count=1, label="YTD", step="year", stepmode="todate"),
             dict(count=1, label="1y", step="year", stepmode="backward"),
-            dict(step="all")
+            #dict(step="all")
         ])
     ))
 
