f81739aa1395305b2205df3bf46ea687dd53750e
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-21 19:39:23 -0500

    fixed the issue with apply as apply map is deprcated.

--- pages/CANDLESTICK_PATTERN_SCREENER.py
+++ pages/CANDLESTICK_PATTERN_SCREENER.py
@@ -245,7 +245,7 @@ with st.container(border=False):
     # Style the DataFrame
     styled_df = (
     df.style.format("{:.2f}", subset=df.select_dtypes(include=["float64", "int64"]).columns)
-    .map(style_table, subset=["Change%", "Net Change"],axis=1)
+    .apply(lambda row:row.apply(style_table), subset=["Change%", "Net Change"],axis=1)
     )
 
 # Display the table in Streamlit
