558ef90a4c44c81c197d2780d6023f7b01ebbffd
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-21 23:39:51 -0500

    hovermode

--- pythonfiles/plotlyplot.py
+++ pythonfiles/plotlyplot.py
@@ -86,6 +86,6 @@ def get_respective_pattern(temp_df,pattern_name):
     )
 #fig update
   fig.update_layout(showlegend=True)
-  fig.update_layout(hovermode='x_unified')
+  fig.update_layout(hovermode='x unified')
 
   return fig
